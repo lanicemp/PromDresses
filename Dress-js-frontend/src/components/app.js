2db0bedc47458a12b7613d231ae5d3f0aaded0a7
LaNice Powell <lanicemp@aol.com>
2020-09-02 19:40:18 -0400

    created class and constuctor

---
+++ Dress-js-frontend/src/components/app.js
@@ -0,0 +1,21 @@
+class App {
+    constructor() {
+       console.log('app loaded, Im in app.js')
+      this.dresses = new Dresses();
+      // this.initiBindingsAndEventListeners()
+  
+    }
+    // initiBindingsAndEventListeners(){
+    //   const addMovieModal = document.getElementById("add-modal");
+    //   const startAddMovieButton = document.querySelector("header button");
+    //   const backdrop = document.getElementById("backdrop");
+  
+    //   // startAddMovieButton.addEventListener("click", showMovieModal);
+    //   // backdrop.addEventListener("click", backdropClickHandler);
+    //   const toggleBackdrop = () => {
+    //       backdrop.classList.toggle("visible");
+    //     };
+    // }
+    
+  }
+  
