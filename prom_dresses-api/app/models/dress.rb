a3c864c4fb8fc7ef30633209e153250ceeea67bb
LaNice Powell <lanicemp@aol.com>
2020-09-09 08:59:22 -0400

    added validations

--- prom_dresses-api/app/models/dress.rb
+++ prom_dresses-api/app/models/dress.rb
@@ -1,4 +1,6 @@
 class Dress < ApplicationRecord
     has_many :ratings, foreign_key: :dress_id
 
+    validates :name, :presence => true, uniqueness: true
+    validates :img_url, :presence => true, uniqueness: true
 end
