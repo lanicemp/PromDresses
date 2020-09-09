03d2c719b27ca1321426e5e4f8c3ebfe735eb946
LaNice Powell <lanicemp@aol.com>
2020-09-09 08:52:34 -0400

    testing event listeners

--- Dress-js-frontend/src/components/dress.js
+++ Dress-js-frontend/src/components/dress.js
@@ -9,11 +9,24 @@ class Dress {
       this.color = dressJSON.color;
       this.img_url = dressJSON.img_url;
       this.dress_id = dressJSON.dress_id;
-      this.price = dressJSON.price
+      this.price = dressJSON.price;
+      // this.dressBindingsAndEventListeners();
+      
+    //   this.comment = ratingJSON.comment
     }
   
     renderLi() {
       return `<li>${this.name}, ${this.silhouette}, ${this.neckline},${this.length}, ${this.color}, ${this.img_url},${this.dress_id}, ${this.price} </li>`;
     }
+    // dressBindingsAndEventListeners() {
+    //   this.viewDressButton = document.querySelector("main button");
+    //   this.viewDress.addEventListener("click", this.viewDressModal);
+    // }
+    // viewDressModal = () => {
+    //   console.log("im in viewDressModal");
+    //   this.addDressModal.classList.add("visible");
+    //   this.toggleBackdrop();
+    // };
+
   }
   
