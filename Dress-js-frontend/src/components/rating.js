5bf057ca80eb4750b55ee2e675df8e4ac954af86
LaNice Powell <lanicemp@aol.com>
2020-09-10 15:54:39 -0400

    changed the constructor

--- Dress-js-frontend/src/components/rating.js
+++ Dress-js-frontend/src/components/rating.js
@@ -1,8 +1,18 @@
 class Rating {
-    constructor(comment, star_rating, dress_id, user_name ){
-        this.comment = comment
-        this.star_rating = star_rating
-        this.dress_id = dress_id
-        this.user_name = user_name
+    constructor(ratingJSON) {
+        console.log("Im in rating.js");
+        this.comment = ratingJSON.comment;
+        this.star_rating = ratingJSON.star_rating;
+        this.dress_id = ratingJSON.dress_id;
+        this.user_name = ratingJSON.user_name
+        
     }
+
+    // constructor(comment, star_rating, dress_id, user_name ){
+    //     console.log("Im in rating.js")
+    //     this.comment = comment
+    //     this.star_rating = star_rating
+    //     this.dress_id = dress_id
+    //     this.user_name = user_name
+    // }
 }
