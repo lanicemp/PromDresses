0df42fea62d440d188b3afc04b78a374f81d786f
LaNice Powell <lanicemp@aol.com>
2020-09-09 22:33:12 -0400

    created rating class

---
+++ Dress-js-frontend/src/components/rating.js
@@ -0,0 +1,8 @@
+class Rating {
+    constructor(comment, star_rating, dress_id, user_name ){
+        this.comment = comment
+        this.star_rating = star_rating
+        this.dress_id = dress_id
+        this.user_name = user_name
+    }
+}
