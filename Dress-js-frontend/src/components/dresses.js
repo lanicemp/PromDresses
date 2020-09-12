36a72ba33bdbe04c1061e4e6eccfd4930c4fd40d
LaNice Powell <lanicemp@aol.com>
2020-09-12 11:40:13 -0400

    cleaned up code and added walkthrough notes

--- Dress-js-frontend/src/components/dresses.js
+++ Dress-js-frontend/src/components/dresses.js
@@ -1,14 +1,16 @@
 class Dresses {
   constructor() {
     this.dresses = [];
-    //  this.rating = [];
+    //This is my dresses database 
     this.baseUrl = "http://localhost:3000/api/v1/dresses";
+    //This is my ratings database 
     this.ratingUrl = "http://localhost:3000/api/v1/ratings";
     // this.adapter = new DressesAdapter();
     this.initiBindingsAndEventListeners();
     this.fetchAndLoadDresses();
   }
   initiBindingsAndEventListeners() {
+    // Im assigning the variables from the HTML 
     this.addDressModal = document.getElementById("add-modal");
     this.startAddDressButton = document.querySelector("header button");
     this.backdrop = document.getElementById("backdrop");
@@ -22,28 +24,19 @@ class Dresses {
     this.dressForm = document.getElementById("new-dress-form");
     this.dressContainer = document.getElementById("dress-container");
     this.viewDressModal = document.getElementById("view-dress-modal");
-
+    //Added eventlisteners to the submit and click buttons
     this.dressForm.addEventListener("submit", this.createNewDress.bind(this));
-    this.dressContainer.addEventListener(
-      "click",
-      this.showDressModal.bind(this)
-    );
-    this.startAddDressButton.addEventListener(
-      "click",
-      this.createDressModal.bind(this)
-    );
+    this.dressContainer.addEventListener("click",this.showDressModal.bind(this));
+    this.startAddDressButton.addEventListener("click",this.createDressModal.bind(this));
     this.backdrop.addEventListener("click", this.backdropClickHandler);
-
-    //  window.dressesContainer.addEventListener('click', this.handleClick.bind(this))
-    // .getElementById('dress-container').addEventListener('click', this.handleClick.bind(this));
   }
+
   toggleBackdrop = () => {
     backdrop.classList.toggle("visible");
   };
 
   createDressModal = () => {
     console.log("im in createDressModal");
-    // function() {}
     this.addDressModal.classList.add("visible");
     this.toggleBackdrop();
   };
@@ -51,7 +44,6 @@ class Dresses {
   closeDressModal = () => {
     this.addDressModal.classList.remove("visible");
     this.viewDressModal.classList.remove("visible");
-
     this.toggleBackdrop();
   };
 
@@ -63,7 +55,8 @@ class Dresses {
   createNewDress(e) {
     e.preventDefault();
     console.log(this);
-    //  this.newDressSilhouette.value;
+    
+    //Im creating the object of dress that has six attributes. 
     const dress = {
       name: this.newDressName.value,
       silhouette: this.newDressSilhouette.value,
@@ -72,6 +65,7 @@ class Dresses {
       price: this.newDressPrice.value,
       length: this.newDressLength.value,
     };
+    //Im opening the database changing the object to a sting and posting the data from the form in the database 
     fetch(this.baseUrl, {
       method: "POST",
       headers: {
@@ -81,12 +75,13 @@ class Dresses {
     })
       .then((res) => res.json())
 
-      // this.adapter
-      //   .createDress(name, silhouette, neckline, img_url, price, length)
       .then((dress) => {
+
         console.log("in create dress");
         console.log(dress);
+        //Pushing the new dress into the array of dresses
         this.dresses.push(new Dress(dress));
+
         this.newDressName.value = " ";
         this.newDressSilhouette.value = " ";
         this.newDressNeckline.value = " ";
@@ -115,91 +110,23 @@ class Dresses {
   render() {
     this.dressContainer.innerHTML = this.dresses
       .map(dress =>dress.renderLi(false))
-      //     ` <div class="gallery">
-      //       <ul class="dress_card" id="${dress.id}">
-      //       <div class = "dress_img"  >
-      //       <img src ="${dress.img_url}" >
-      //       </div>
-      //       <div class = "dress_info"> 
-      //       <h3>${dress.name}<h3>
-      //       <td>
-      //         <div class="stars-outer">
-      //           <div class="stars-inner"></div>
-      //         </div>
-      //         <span class="number-rating"></span>
-      //       </td>
-      //       <br>
-      //       <br>
-      //       </div>
-      //        <button> Dress Details and Comments </button>
-             
-            
-      //       </ul>
-      //       </div>
-      //       `
-      // )
       .join("");
+
+
   }
   
   showDressModal = (e) => {
     console.log("im in showDressModal");
-      //  this.fetchAndLoadRatings();
- 
     console.log(e);
     const dress_id = parseInt(e.target.parentElement.id);
     console.log(typeof dress_id);
     let dress = this.dresses.filter((dress) => dress.id === dress_id);
     console.log(dress);
     dress = dress[0];
-  
-    
+
     this.viewDressModal.innerHTML = dress.renderLi(true);
-    // `<div class = "dress_img"  >
-    //         <img src ="${dress.img_url}" >
-    //         </div>
-    //         <div class = "dress_info"> 
-    //         <h2>${dress.name}<h2>
-    //         <td>
-    //           <div class="stars-outer">
-    //             <div class="stars-inner"></div>
-    //           </div>
-    //           <span class="number-rating"></span>
-    //         </td>
-    //         <p> silhouette: ${dress.silhouette}
-    //         <br> neckline: ${dress.neckline} 
-    //         <br> length: ${dress.length}
-    //         <br> price: $${dress.price}</p>
-    //         <br>
-    //         <h2> Rate This Dress </h2>
-
-    //         <form id="new-rating-form">
-    //           <label for="userName "> User Name  </label>
-    //           <input type="text" name="userName" id="userName " />
-    //           <label for="rating ">Rating </label>
-                       
-    //           <select name="rating " id="rating " >
-    //             <option value="1">1</option>
-    //             <option value="2">2</option>
-    //             <option value="3">3</option>
-    //             <option value="4">4</option>
-    //             <option value="5">5</option>
-    //           </select>
-
-    //           <label for="comment"> Comment </label>
-    //           <input type="text" name="comment" id="comment" />
-    //           <input type="submit" value="Save Rating" />
-
-    //         </form>
-    //         <ul>
-    //         ${ratings};
-    //         </ul>
-    //         </div>
-
-    //         <br>
-    //         `;
     this.ratingForm = document.getElementById("new-rating-form");
     this.ratingForm.addEventListener("submit", this.createNewRating.bind(this));
-    
     this.viewDressModal.classList.add("visible");
     this.toggleBackdrop();
   };
@@ -229,7 +156,7 @@ class Dresses {
       .then((res) => res.json())
 
       .then((rating) => {
-        console.log("in create rating");
+        console.log("in create new rating");
         console.log(rating);
         const newRating = new Rating(rating);
         this.dresses.forEach(dress => {
@@ -243,7 +170,7 @@ class Dresses {
 
         this.render();
       });
-    this.closeDressModal();
+    // this.closeDressModal();
     console.log(rating);
   }
   fetchAndLoadRatings() {
