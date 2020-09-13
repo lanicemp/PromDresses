ae442d2f8f32ed6ed6a4d13453ec3b5ed013e527
LaNice Powell <lanicemp@aol.com>
2020-09-13 02:48:00 -0400

    Moved getRatings to dress class

--- Dress-js-frontend/src/components/rating.js
+++ Dress-js-frontend/src/components/rating.js
@@ -16,25 +16,26 @@ class Rating {
         this.username = username
     }
 
-    getRatings() {
-      
-        console.log( "im in rating.get ratings")
-       
-       //  console.log(ratings)
-       for (let rating in ratings) {
-         // Get percentage
-         const starPercentage = (ratings[rating] / starsTotal) * 100;
+    //  getRatings(rating) {
+    //   console.log(rating)
+    //    console.log( "im in rating.get ratings")
+    //    const starsTotal = 5 
+    // //    //  console.log(ratings)
+    // //    for (let rating in ratings) {
+    // //      // Get percentage
+    // // const starPercentage = (ratings[rating] / starsTotal) * 100;
+    //      const starPercentage = (rating / starsTotal) * 100;
  
-         // Round to nearest 10
-         const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+    // //      // Round to nearest 10
+    // //      const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
  
-         // Set width of stars-inner to percentage
-         document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+    // //      // Set width of stars-inner to percentage
+    // //      document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+    // document.querySelector(`.${rating.id} .stars-inner`).style.width = starPercentage;
+    // //      // Add number rating
+    //      document.querySelector(`.${rating.id} .number-rating`).innerHTML = rating;
  
-         // Add number rating
-         document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
- 
-         this.renderDressRatings(this.ratings)
-       }
-     } 
+    // //      this.renderDressRatings(this.ratings)
+    // //    }
+    //  } 
 }
