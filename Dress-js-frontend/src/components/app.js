f18c6ce6a4be18c2ed7bfe885595d64996184182
LaNice Powell <lanicemp@aol.com>
2020-09-19 12:50:25 -0400

    Cleaned up code and added notes

--- Dress-js-frontend/src/components/app.js
+++ Dress-js-frontend/src/components/app.js
@@ -1,21 +1,8 @@
 class App {
     constructor() {
        console.log('app loaded, Im in app.js')
-      this.dresses = new Dresses();
-      // this.initiBindingsAndEventListeners()
-  
-    }
-    // initiBindingsAndEventListeners(){
-    //   const addMovieModal = document.getElementById("add-modal");
-    //   const startAddMovieButton = document.querySelector("header button");
-    //   const backdrop = document.getElementById("backdrop");
-  
-    //   // startAddMovieButton.addEventListener("click", showMovieModal);
-    //   // backdrop.addEventListener("click", backdropClickHandler);
-    //   const toggleBackdrop = () => {
-    //       backdrop.classList.toggle("visible");
-    //     };
-    // }
-    
+      this.dresses = new Dresses()
+  //Created the new object of Dresses
+    } 
   }
   
