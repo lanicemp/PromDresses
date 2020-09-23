940cd2cfb8f7e8e90ff710941c1ac42a9e9214ee
LaNice Powell <lanicemp@aol.com>
2020-09-23 10:27:08 -0400

    created a function to be able to gather the average of ratings from back end.

--- prom_dresses-api/app/models/dress.rb
+++ prom_dresses-api/app/models/dress.rb
@@ -3,4 +3,15 @@ class Dress < ApplicationRecord
 
     validates :name, :presence => true, uniqueness: true
     validates :img_url, :presence => true, uniqueness: true
+
+
+ def self.get_average_rating 
+    # Call on the ratings to be able to calculate the average rating.  Dress.ratings should work. 
+    binding.pry 
+ end 
+
+
+
+
+
 end
